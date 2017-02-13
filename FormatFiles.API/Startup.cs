fa5902f7c71d4eb8b56928a2fcfa9fd5195697d5
chuckwang123 <wangjohn419@gmail.com>
2017-02-13 13:37:35 -0600

    Change the default mapping route to swagger

--- FormatFiles.API/Startup.cs
+++ FormatFiles.API/Startup.cs
@@ -10,12 +10,21 @@ namespace FormatFiles.API
         public void Configuration(IAppBuilder app)
         {
             var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.Routes.MapHttpRoute(
+            "swagger_root",
+            "",
+            null,
+            null,
+            new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
+
+
             WebApiConfig.Register(httpConfiguration);
             httpConfiguration.EnsureInitialized();
             httpConfiguration.EnableSwagger(x=>x.SingleApiVersion("v1","Format Files API"))
                 .EnableSwaggerUi();
             app.UseCors(CorsOptions.AllowAll)
                .UseWebApi(httpConfiguration);
+
         }
     }
 }
